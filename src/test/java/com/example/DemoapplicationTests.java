b13f544ca1c9775d9aff4ebc6d4bf1135f6c9214
leanhthanhtai <leanhthanhtai@gmail.com>
2021-10-24 22:28:08 +0700

    API_Gate

---
+++ src/test/java/com/example/DemoapplicationTests.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@SpringBootTest
+public class DemoapplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
